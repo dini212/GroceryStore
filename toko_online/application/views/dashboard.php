ad2b4bb1d60221b9c5bd61ffa90e0955c99d07bf
Amalia-Salsabilla <amalia.salsa28@gmail.com>
2021-12-14 15:57:15 +0700

    mengupdate kodingan amalia

--- toko_online/application/views/dashboard.php
+++ toko_online/application/views/dashboard.php
@@ -1,11 +1,10 @@
 <div class="container-fluid">
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active" ></li>
+             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+         </ol> 
           <div class="carousel-inner">
             <div class="carousel-item active">
               <img src="<?php echo base_url('assets/img/slider1.jpg')?>" class="d-block w-100" alt="...">
@@ -13,16 +12,7 @@
             <div class="carousel-item">
               <img src="<?php echo base_url('assets/img/slider2.jpg')?>" class="d-block w-100" alt="...">
             </div>
-            
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
         </div>
 
 	<div class="row text-center mt-3">
