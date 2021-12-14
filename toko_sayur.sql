89663708a86cfbdde952b4bc36161f5268b40fe3
dini212 <dwirahmadini1234@gmail.com>
2021-12-14 18:09:15 +0700

    update db

--- toko_sayur.sql
+++ toko_sayur.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2021 at 01:19 PM
+-- Generation Time: Dec 14, 2021 at 11:57 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -43,8 +43,13 @@ CREATE TABLE `sayuran` (
 
 INSERT INTO `sayuran` (`id_sayuran`, `nama_sayuran`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
 (1, ' Bayam', 'harga/ikat', 'Sayur Hijau', 1000, 15, 'bayam.jpg'),
-(3, 'Sawi', 'harga/ikat', 'Sayur Hijau', 2000, 10, 'sawi.jpg'),
-(4, 'Wortel', 'harga/kg', 'umbi - umbian', 18000, 10, 'wortel.jpg');
+(3, 'Sawi', 'harga/ikat', 'Sayur Hijau', 2000, 10, 'Sawi.jpg'),
+(4, 'Wortel', 'harga/kg', 'umbi - umbian', 18000, 10, 'Wortel.jpg'),
+(7, 'Tomat', 'harga/kg', '-', 10000, 12, 'tomat.jpg'),
+(8, 'Cabai', 'harga/ons', '-', 7500, 5, 'cabairawit.jpg'),
+(11, 'Brokoli', 'harga/plastik(1 bongkah/plastik)', '-', 5000, 10, 'brokoli.jpg'),
+(12, 'Kangkung', 'harga/ikat', 'sayur hijau', 2000, 10, 'kangkung.jpg'),
+(14, 'Cabai besar', 'harga/ons', '-', 6000, 5, 'Cabaibesar.jpg');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +69,7 @@ ALTER TABLE `sayuran`
 -- AUTO_INCREMENT for table `sayuran`
 --
 ALTER TABLE `sayuran`
-  MODIFY `id_sayuran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_sayuran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
